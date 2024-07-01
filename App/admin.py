471b61bf1e6982ad0491407953b7895238c27a96
binayrijal <binayrijal22@gmail.com>
2024-07-01 13:08:53 +0545

    added graph view in admin panel

--- App/admin.py
+++ App/admin.py
@@ -4,6 +4,95 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import User,Event,Trashdata,Notification,FeedBack
 from django import forms
+from django.urls import path
+from django.template.response import TemplateResponse
+from django.db.models import Sum
+
+
+class MyAdminSite(admin.AdminSite):
+    
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('trashdata_chart/', self.admin_view(self.trashdata_chart_view), name='trashdata_chart')
+        ]
+        return custom_urls + urls
+
+    def trashdata_chart_view(self, request):
+        if not request.user.is_superuser:
+            self.message_user(request, "You do not have permission to view this page.", level="error")
+            return TemplateResponse(request, "admin/permission_denied.html")
+        
+        data = Trashdata.objects.order_by('timestamp').values('timestamp').annotate(total_trash=Sum('trash'))
+        
+        timestamps = [item['timestamp'].strftime("%Y-%m-%d") for item in data]
+        trash_values = [item['total_trash'] for item in data]
+        
+        location_data = Trashdata.objects.values('location').annotate(total_trash=Sum('trash'))
+        locations = [item['location'] for item in location_data]
+        trash_values_by_location = [item['total_trash'] for item in location_data]
+        
+        context = dict(
+            self.each_context(request),
+            data={
+                'timestamps': timestamps,
+                'trash_values': trash_values,
+                'locations': locations,
+                'trash_values_by_location': trash_values_by_location
+            }
+        )
+        return TemplateResponse(request, "admin/trashdata_chart.html", context)
+
+admin_site = MyAdminSite()
+# class MyAdminSite(admin.AdminSite):
+#     index_template='admin/base_site.html'
+#     def get_urls(self):
+#         urls = super().get_urls()
+#         custom_urls = [
+#             path('trashdata_chart/', self.admin_view(self.trashdata_chart_view), name='trashdata_chart')
+#         ]
+#         return custom_urls + urls
+
+#     def trashdata_chart_view(self, request):
+#         if not request.user.is_superuser:
+#             self.message_user(request, "You do not have permission to view this page.", level="error")
+#             return TemplateResponse(request, "admin/permission_denied.html")
+        
+#         data = Trashdata.objects.order_by('timestamp').values('timestamp').annotate(total_trash=Sum('trash'))
+        
+#         timestamps = [item['timestamp'].strftime("%Y-%m-%d") for item in data]
+#         trash_values = [item['total_trash'] for item in data]
+        
+#         location_data = Trashdata.objects.values('location').annotate(total_trash=Sum('trash'))
+#         locations = [item['location'] for item in location_data]
+#         trash_values_by_location = [item['total_trash'] for item in location_data]
+        
+#         context = dict(
+#             self.each_context(request),
+#             data={
+#                 'timestamps': timestamps,
+#                 'trash_values': trash_values,
+#                 'locations': locations,
+#                 'trash_values_by_location': trash_values_by_location
+#             }
+#         )
+#         return TemplateResponse(request, "admin/trashdata_chart.html", context)
+
+# admin_site = MyAdminSite()
+
+# @admin.register(Trashdata)
+# class TrashdataAdmin(admin.ModelAdmin):
+#     change_list_template = "admin/change_list.html"
+
+#     def changelist_view(self, request, extra_context=None):
+#         extra_context = extra_context or {}
+#         return super().changelist_view(request, extra_context=extra_context)
+    
+
+
+
+
+
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -60,25 +149,25 @@ class UserModelAdmin(UserAdmin):
 
 
 # Now register the new UserAdmin...
-admin.site.register(User, UserModelAdmin)
+admin_site.register(User, UserModelAdmin)
 # ... and, since we're not using Django's built-in permissions,
 # unregister the Group model from admin.
 #admin.site.unregister(Group)
 
 
-@admin.register(Event)
+    
+@admin.register(Event, site=admin_site)
 class EventAdmin(admin.ModelAdmin):
     list_display = ('title', 'start_time', 'end_time')
     
-@admin.register(Trashdata)
+@admin.register(Trashdata, site=admin_site)
 class TrashAdmin(admin.ModelAdmin):
-    list_display=('id','location','trash','timestamp')
+    list_display = ('id', 'location', 'trash', 'timestamp')
     
-@admin.register(Notification)
+@admin.register(Notification, site=admin_site)
 class NotificationAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'sent_at', 'lati', 'longi')
     
-@admin.register(FeedBack)
+@admin.register(FeedBack, site=admin_site)
 class FeedBackAdmin(admin.ModelAdmin):
-    list_display=('id','user','feedback_msg','rating')
-    
+    list_display = ('id', 'user', 'feedback_msg', 'rating')
